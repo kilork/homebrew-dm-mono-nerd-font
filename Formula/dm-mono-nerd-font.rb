280bd3e7d19db4b6815c3299fe72b5354db1060b
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-03-21 22:29:02 +0100

    Fix install method

--- Formula/dm-mono-nerd-font.rb
+++ Formula/dm-mono-nerd-font.rb
@@ -7,12 +7,7 @@ class DmMonoNerdFont < Formula
   sha256 "1bd78369eb35c7a735d6269f6d6b36b41bebabdb540c0f291e8babd83fbe6f6f"
 
   def install
-    font_dir = Pathname.new(ENV["HOME"])/"Library/Fonts"
-    font_dir.mkpath
-
-    Dir["*.ttf"].each do |font|
-      cp font, font_dir
-    end
+    ( HOMEBREW_PREFIX / "Library/Fonts" ).install Dir["*"]
   end
 
   test do
