a1ce935f1f6248d9885e866b5750d139de80dd9b
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-03-21 22:31:13 +0100

    Fix install to use share/fonts

--- Formula/dm-mono-nerd-font.rb
+++ Formula/dm-mono-nerd-font.rb
@@ -7,12 +7,13 @@ class DmMonoNerdFont < Formula
   sha256 "1bd78369eb35c7a735d6269f6d6b36b41bebabdb540c0f291e8babd83fbe6f6f"
 
   def install
-    (share / "fonts").install Dir["dm-mono-nerd-font-1.0.0/*"]
+    (share / "fonts").install Dir["*"]
   end
 
   def post_install
     ohai "Installing fonts to ~/Library/Fonts..."
-    (HOMEBREW_PREFIX / "Library/Fonts").cp Dir[prefix/"fonts/*"]
+    (HOMEBREW_PREFIX / "Library/Fonts").mkdir_p
+    (HOMEBREW_PREFIX / "Library/Fonts").cp Dir[share/"fonts/*"]
   end
 
   test do
