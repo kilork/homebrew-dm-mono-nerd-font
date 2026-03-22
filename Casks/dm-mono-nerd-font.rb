bb43e94949ed3a743fadd9898c5fa8957078e102
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-03-22 11:53:20 +0100

    Fix SHA256

--- Casks/dm-mono-nerd-font.rb
+++ Casks/dm-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "dm-mono-nerd-font" do
   version "3.4.0"
-  sha256 "test123"
+  sha256 "c52fd12cedabaa582dbd5c4d24ac931b321d02bba6fa14b4c04e67b58bf155b2"
 
   url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v3.4.0/dm-mono-nerd-font-3.4.0.tar.gz"
   name "DM Mono Nerd Font"
