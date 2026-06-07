9e6d67fafe86a25b04e5a7a20aaad055a342e155
GitHub Action <action@github.com>
2026-06-07 15:34:34 +0000

    Update cask to v1.0.0

--- Casks/dm-mono-nerd-font.rb
+++ Casks/dm-mono-nerd-font.rb
@@ -1,8 +1,8 @@
 cask "dm-mono-nerd-font" do
-  version "3.4.0"
-  sha256 "38cefb6422ae2092d06bd2ad52062473b16e935b56c150573edc7c70145bd211"
+  version "1.0.0"
+  sha256 "c64774023eab6e89d87ae7c4da80e561af0e857af351d49dd7dd56cc82d98b97"
 
-  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v3.4.0/dm-mono-nerd-font-3.4.0.tar.gz"
+  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v1.0.0/dm-mono-nerd-font-1.0.0.tar.gz"
   name "DM Mono Nerd Font"
   homepage "https://github.com/kilork/dm-mono-nerd-font"
 
